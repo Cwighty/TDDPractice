279199dde00f488e506bd401690498ae0209aa59
Caleb Wightman <agentcboy@gmail.com>
2023-03-18 11:18:01 -0600

    refactor

--- ClassLibrary/PrimeFactors.cs
+++ ClassLibrary/PrimeFactors.cs
@@ -5,16 +5,11 @@ public class PrimeFactors
     public static List<int> Generate(int number)
     {
         var factors = new List<int>();
-        int divisor = 2;
-        while (number > 1)
-        {
-            while (number % divisor == 0)
-            {
+
+        for (int divisor = 2; number > 1; divisor++)
+            for (; number % divisor == 0; number /= divisor)
                 factors.Add(divisor);
-                number /= divisor;
-            }
-            divisor++;
-        }
+
         return factors;
     }
 }
